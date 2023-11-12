787546ec8331fce9f1f5ab3891bdd948598d780a
João Válter Arruda <joaovalterarruda@gmail.com>
2023-11-12 16:57:03 -0100

    departmend_id working

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -4,9 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Department;
-use App\Models\Employee;
 use App\Models\User;
-use App\Models\PunchEvent;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\App;
 
