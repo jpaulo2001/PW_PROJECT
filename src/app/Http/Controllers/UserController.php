d94680a6f88e9f16ff0a31a5c30de77f2e599f5e
João Válter Arruda <joaovalterarruda@gmail.com>
2023-11-18 16:24:33 -0100

    add userController...

---
+++ src/app/Http/Controllers/UserController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Auth\Controller;
+use App\Http\Requests\UpdateEmployeeRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+//    public function __construct()
+//    {
+//        $this->authorizeResource(User::class, 'user');
+//    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $users = User::orderBy('name')->paginate(25);
+        return view(
+            'users.index',
+            [
+                'users' => $users
+            ]
+        );
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        return view(
+            'users.show',
+            [
+                'user' => $user
+            ]
+        );
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(User $user)
+    {
+        return view(
+            'users.edit',
+            [
+                'user' => $user
+            ]
+        );
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateEmployeeRequest $request, User $user)
+    {
+        $user->update($request->toDTO()->toArray());
+        return redirect()
+            ->route('users.show', ['user' => $user]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+}
