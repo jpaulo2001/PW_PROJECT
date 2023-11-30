5d01262da8d40e9693699f4dbe2661ecbeb320df
João Válter Arruda <joaovalterarruda@gmail.com>
2023-11-30 19:19:06 -0100

    policias, ver e modificar apenas admin

--- src/resources/views/users/index.blade.php
+++ src/resources/views/users/index.blade.php
@@ -4,15 +4,16 @@
     <div class="container">
         <div class="row mt-5">
             <div class="col">
-                <a href="{{ route('users.create') }}"
-                   class="btn btn-success btn-sm mb-3">Criar</a>
+                @can('create', App\Models\User::class) // está assim porque não conseguia o user e foi maneira de trabalhar...ver mais a frente
+                    <a href="{{ route('users.create') }}" class="btn btn-success btn-sm mb-3">Criar</a>
+                @endcan
 
                 <table class="table table-striped table-hover">
                     <thead>
                     <tr>
                         <th>Nome</th>
                         <th>Email</th>
-                        <th class="text-end">Acções</th>
+                        <th class="text-end">Ações</th>
                     </tr>
                     </thead>
                     <tbody>
@@ -21,12 +22,9 @@
                             <td>{{ $user->name }}</td>
                             <td>{{ $user->email }}</td>
                             <td class="text-end">
-                                <a href="{{ route('users.show', ['user' => $user]) }}"
-                                   class="btn btn-primary btn-sm">Ver</a>
-
-                                @can('update', $user)
-                                    <a href="{{ route('users.edit', ['user' => $user]) }}"
-                                       class="btn btn-warning btn-sm">Modificar</a>
+                                <a href="{{ route('users.show', ['user' => $user]) }}" class="btn btn-primary btn-sm">Ver</a>
+                                @can('create', App\Models\User::class)
+                                    <a href="{{ route('users.edit', ['user' => $user]) }}" class="btn btn-warning btn-sm">Modificar</a>
                                 @endcan
                             </td>
                         </tr>
