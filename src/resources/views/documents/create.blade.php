b164410b6b38edaa407b3fd07a58ee9a3a8747b3
João Válter Arruda <joaovalterarruda@gmail.com>
2023-12-01 19:20:03 -0100

    view meio fossada, mas quero que a lógica funcione, só mesmo para testes

--- src/resources/views/documents/create.blade.php
+++ src/resources/views/documents/create.blade.php
@@ -10,23 +10,216 @@
         Tipo documento: <input type="text" name="document" id="" class="form-control"><br>
         @error('tipo documento') <span class="text-danger">{{ $message }}</span><br>@enderror
 
-        Permissões do documento: <br>
+{{--        Permissões do documento: <br>--}}
 
-        <input class="form-check-input-inline" type="radio" name="flexRadioDefault">
-            <label class="form-check-inline" for="flexRadioDefault1">
-                Editar
-            </label><br>
-        <input class="form-check-input-inline" type="radio" name="flexRadioDefault">
-            <label class="form-check-inline" for="flexRadioDefault2">
-                Ver
-            </label><br>
-        <input class="form-check-input-inline" type="radio" name="flexRadioDefault">
-            <label class="form-check-inline" for="flexRadioDefault2">
-                Remover
+{{--        <input class="form-check-input-inline" type="radio" name="flexRadioDefault">--}}
+{{--            <label class="form-check-inline" for="flexRadioDefault1">--}}
+{{--                Editar--}}
+{{--            </label><br>--}}
+{{--        <input class="form-check-input-inline" type="radio" name="flexRadioDefault">--}}
+{{--            <label class="form-check-inline" for="flexRadioDefault2">--}}
+{{--                Ver--}}
+{{--            </label><br>--}}
+{{--        <input class="form-check-input-inline" type="radio" name="flexRadioDefault">--}}
+{{--            <label class="form-check-inline" for="flexRadioDefault2">--}}
+{{--                Remover--}}
+{{--            </label>--}}
+{{--        @error('name') <span class="text-danger">{{ $message }}</span><br>@enderror<br>--}}
+        <b>Permissões do departamento:</b><br>
+        <b>Escolher departamento: </b>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
+            <label class="form-check-label" for="flexCheckDefault">
+                Atendimento ao Cliente
             </label>
-        @error('name') <span class="text-danger">{{ $message }}</span><br>@enderror<br>
-
-        <button type="submit" class="btn btn-success btn-lg">Guardar Modificações</button>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
+                <label class="form-check-label" for="flexCheckDefault">
+                    Editar
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
+                <label class="form-check-label" for="flexCheckDefault">
+                    Ver
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
+                <label class="form-check-label" for="flexCheckDefault">
+                    Remover
+                </label>
+            </div>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
+            <label class="form-check-label" for="flexCheckDefault">
+                Contabilidade
+            </label>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
+                <label class="form-check-label" for="flexCheckDefault">
+                    Editar
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
+                <label class="form-check-label" for="flexCheckDefault">
+                    Ver
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
+                <label class="form-check-label" for="flexCheckDefault">
+                    Remover
+                </label>
+            </div>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
+            <label class="form-check-label" for="flexCheckDefault">
+                Logística
+            </label>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
+                <label class="form-check-label" for="flexCheckDefault">
+                    Editar
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
+                <label class="form-check-label" for="flexCheckDefault">
+                    Ver
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
+                <label class="form-check-label" for="flexCheckDefault">
+                    Remover
+                </label>
+            </div>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
+            <label class="form-check-label" for="flexCheckDefault">
+                Marketing
+            </label>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
+                <label class="form-check-label" for="flexCheckDefault">
+                    Editar
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
+                <label class="form-check-label" for="flexCheckDefault">
+                    Ver
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
+                <label class="form-check-label" for="flexCheckDefault">
+                    Remover
+                </label>
+            </div>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
+            <label class="form-check-label" for="flexCheckDefault">
+                Pesquisa e Desenvolvimento
+            </label>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
+                <label class="form-check-label" for="flexCheckDefault">
+                    Editar
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
+                <label class="form-check-label" for="flexCheckDefault">
+                    Ver
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
+                <label class="form-check-label" for="flexCheckDefault">
+                    Remover
+                </label>
+            </div>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
+            <label class="form-check-label" for="flexCheckDefault">
+                Produção
+            </label>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
+                <label class="form-check-label" for="flexCheckDefault">
+                    Editar
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
+                <label class="form-check-label" for="flexCheckDefault">
+                    Ver
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
+                <label class="form-check-label" for="flexCheckDefault">
+                    Remover
+                </label>
+            </div>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
+            <label class="form-check-label" for="flexCheckDefault">
+                Recursos Humanos
+            </label>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
+                <label class="form-check-label" for="flexCheckDefault">
+                    Editar
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
+                <label class="form-check-label" for="flexCheckDefault">
+                    Ver
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
+                <label class="form-check-label" for="flexCheckDefault">
+                    Remover
+                </label>
+            </div>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
+            <label class="form-check-label" for="flexCheckDefault">
+                Tecnologia da Informação
+            </label>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
+                <label class="form-check-label" for="flexCheckDefault">
+                    Editar
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
+                <label class="form-check-label" for="flexCheckDefault">
+                    Ver
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
+                <label class="form-check-label" for="flexCheckDefault">
+                    Remover
+                </label>
+            </div>
+        </div>
+        <br><button type="submit" class="btn btn-success btn-lg">Guardar Modificações</button>
 
 
     </form>
