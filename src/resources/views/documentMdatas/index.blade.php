fa017a42cf7c325dd210a6d1488043696258c786
Henrique Ponte <107682582+henriquePonte@users.noreply.github.com>
2024-01-16 23:24:24 -0100

    permissoes edit e remove

--- src/resources/views/documentMdatas/index.blade.php
+++ src/resources/views/documentMdatas/index.blade.php
@@ -4,33 +4,34 @@
     <div class="container">
         <div class="row mt-5">
             <div class="col">
-                <a href="{{ route('documentMdatas.create') }}" class="btn btn-success btn-sm mb-3">Criar</a>
+                @can('create', App\Models\User::class)
+                    <a href="{{ route('documentMdatas.create') }}" class="btn btn-success btn-sm mb-3">Criar</a>
+                @endcan
                 <table class="table table-striped table-hover">
                     <thead>
                     <tr>
                         <th>Nome</th>
+                        <th>Código</th>
                         <th class="text-end">Ações</th>
                     </tr>
                     </thead>
                     <tbody>
                     @foreach($mdatas as $mdata)
                         <tr>
-                            <th>{{ $mdata->mdata}}</th>
+                            <th>{{ $mdata->mdata }}</th>
                             <td class="text-end">
-{{--                                @can('update', APP\Models\User::class)--}}
+                                @can('update', APP\Models\User::class)
                                     <a href="{{ route('documentMdatas.edit', ['documentMdata' => $mdata->id]) }}"
                                        class="btn btn-warning btn-outline-sm">Modificar</a>
-{{--                                @endcan--}}
-
-{{--                                @can('delete', APP\Models\User::class)--}}
-                                    <form action="{{ route('documentMdatas.destroy', ['documentMdata' => $mdata->id]) }}"
-                                          method="POST" class="d-inline">
+                                @endcan
+                                
+                                @can('delete', APP\Models\User::class)
+                                    <form action="{{ route('documentMdatas.destroy', ['documentMdata' => $mdata->id]) }}" method="POST" class="d-inline">
                                         @csrf
                                         @method('DELETE')
                                         <button type="submit" class="btn btn-delete btn-outline-danger">Eliminar</button>
                                     </form>
-{{--                                @endcan--}}
-
+                                @endcan
                             </td>
                         </tr>
                     @endforeach
