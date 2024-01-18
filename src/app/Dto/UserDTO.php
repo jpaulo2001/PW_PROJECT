5c41bd6837c8afaa3b8cc2fc0595087236638bb4
Henrique Ponte <107682582+henriquePonte@users.noreply.github.com>
2024-01-18 19:05:04 -0100

    DTO update a funcionar user

--- src/app/Dto/UserDTO.php
+++ src/app/Dto/UserDTO.php
@@ -5,11 +5,11 @@ namespace App\Dto;
 class UserDTO
 {
     public string $name;
-    public string $email;
-    public string $password;
+    public ?string $email;
+    public ?string $password;
     public string $department_id;
     public string $user_type_permition_id;
-    public function __construct(string $name, string $email, string $password, string $department_id, string $user_type_permition_id)
+    public function __construct(string $name, ?string $email, ?string $password, string $department_id, string $user_type_permition_id)
     {
         $this->name = $name;
         $this->email = $email;
